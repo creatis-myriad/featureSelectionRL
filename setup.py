5bd328f2a8603eade3ffadcf25688ba3f7eb7783
Gabriel Bernardino <gabriel.bernardino1@gmail.com>
2023-05-28 09:34:30 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'HierarchyRL',
+    version = '0.1',
+    author = 'Gabriel Bernardino',
+    author_email = 'gabriel.bernardino@creatis.insa-lyon.fr',
+    packages=['hierarchyrl'],
+    package_dir = {'' : 'src'}
+)
